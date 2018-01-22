5109cfd668f5fb014ef83a4ef32acfe61637a5ee
xezian <xezian@icloud.com>
2018-01-21 21:29:50 -0700

    hang.js uses the Word constructor to create the word which uses the letter constructor for each letter which contains methods to manipulate the letters within the word

--- hang.js
+++ hang.js
@@ -1,1 +1,14 @@
-const Letter = require("./letter.js").Letter;
+// require word.js
+const Word = require("./word.js").Word;
+// require inquirer.js
+const inquirer = require("inquirer");
+// get a new word using the word constructor (which in turn uses the letter constructor)
+let mysteryWord = new Word();
+mysteryWord.letterize();
+mysteryWord.displayWord();
+let seeTheWord = mysteryWord.wordDisplay.join(" ");
+console.log(seeTheWord);
+mysteryWord.letters[1].guessThis();
+mysteryWord.displayWord();
+seeTheWord = mysteryWord.wordDisplay.join(" ");
+console.log(seeTheWord);
--- word.js
+++ word.js
@@ -1,4 +1,25 @@
 // require Letter constructor function out of which to assemble entire words!
 const Letter = require("./letter.js").Letter;
-
+// random word to get a bunch o rando words yo
+const randomWord = require('random-word');
 // constructor function to create a word out of letters
+const Word = function() {
+    // what's the word
+    this.word = randomWord();
+    // array to contain each letter of the word
+    this.letters = [];
+    this.wordDisplay = [];
+};
+Word.prototype.letterize = function() {
+    for (let i = 0; i < this.word.length; i++) {
+        let newLetter = new Letter(this.word[i]);
+        this.letters.push(newLetter);
+    }
+};
+Word.prototype.displayWord = function() {
+    this.wordDisplay = [];
+    for (let i = 0; i < this.letters.length; i++) {
+        this.wordDisplay.push(this.letters[i].showThis);
+    }
+}
+module.exports.Word = Word;
